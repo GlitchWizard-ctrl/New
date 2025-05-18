d3da64d8a638f364f561e9d910ab578794661ee2
GlitchWizard-ctrl <prarthananagaraj3@gmail.com>
2025-05-18 16:34:32 +0530

    Initial commit

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.post('/greet', (req, res) => {
     res.json({ message: `Hello, ${name}!` });
 });
 
-app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
